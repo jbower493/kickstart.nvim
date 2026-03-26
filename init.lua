70f07de08bad5d20f6395625a37ce4c74d4db690
Jamie Bower <j_bower@hotmail.co.uk>
2026-03-26 19:56:27 +1100

    feat: enable prettier

--- init.lua
+++ init.lua
@@ -609,7 +609,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
 
         stylua = {}, -- Used to format Lua code
 
@@ -700,7 +700,7 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
